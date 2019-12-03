344017e5f090a4d69d81a9c4ef7c786e24f0cbab
Terry chan <napoler2008@gmail.com>
2019-12-04 07:35:26 +0800

    修复gpu训练bug
    Signed-off-by: Terry chan <napoler2008@gmail.com>

--- model/bert_lstm_crf.py
+++ model/bert_lstm_crf.py
@@ -47,7 +47,13 @@ class BERT_LSTM_CRF(nn.Module):
         embeds, _ = self.word_embeds(sentence, attention_mask=attention_mask, output_all_encoded_layers=False)
         hidden = self.rand_init_hidden(batch_size)
         if embeds.is_cuda:
-            hidden = (i.cuda() for i in hidden)
+            print("type=========",type(hidden))
+            print("hidden",hidden)
+            a,b =hidden
+            hidden=a.cuda(),b.cuda()
+            # hidden = (i.cuda() for i in hidden)
+        print("hidden gpu",hidden)
+        # print("hidden",hidden)
         lstm_out, hidden = self.lstm(embeds, hidden)
         lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim*2)
         d_lstm_out = self.dropout1(lstm_out)
