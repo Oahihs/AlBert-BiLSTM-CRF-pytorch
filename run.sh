c830081b70d57d1d9956e0f280e09dac86d89074
Terry chan <napoler2008@gmail.com>
2019-12-04 04:11:13 +0800

    Signed-off-by: Terry chan <napoler2008@gmail.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 CURRENT_DIR=`pwd`
 
-python main.py 
+python main.py train
 # export BERT_BASE_DIR=$CURRENT_DIR/prev_trained_model/albert_tiny
 # export DATA_DIR=$CURRENT_DIR/dataset
 # export OUTPUR_DIR=$CURRENT_DIR/outputs
